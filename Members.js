d15574f9b4f89c3679e7add958d1788f936ddca5
akoeth-portfolio <dukewellington61@googlemail.com>
2020-04-01 00:07:02 +0200

    1st commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@wiliam.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'shannon@jackson.com',
+        status: 'inactive'
+    }
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+// express-handlebars -> template engine
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger.js');
+const members = require('./Members');
+
+
+const app = express();
+
+// Init middleware --> everytime a request is made, this middleware is gonna run
+// app.use(logger);
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'})); /* sets template engine to handlebars, layout is called 'main'  */
+app.set('view engine', 'handlebars'); /* sets view engine to handlebars */
+
+// Body Parser Middleware - to access json data in request-object on post & put requests
+app.use(express.json());
+app.use(express.urlencoded({ extende: false })); /* check out https://expressjs.com/en/api.html for more */
+
+// Homepage Route (if static folder is put above this the index.html in the publich folder is beeing rendered)
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members: members
+}));
+
+// Set static folder
+// app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('public'));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
